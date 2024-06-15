3a7bc89c21e4dbde83b9b9b7ad9f2dc32a07837d
Tarun Chawdhury <tarun@taruns-air.lan>
2024-06-15 13:13:25 -0700

    Added Ansible

--- SmartGridMockApp/stop.sh
+++ SmartGridMockApp/stop.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-if [ -f pid.file ]; then
-    kill $(cat pid.file)
-    rm -f pid.file
-    echo "Application stopped."
+APP_DIR="/home/dlyog/ted/omniagent/SmartGridMockApp"
+PID_FILE="$APP_DIR/pid.file"
+
+if [ -f $PID_FILE ]; then
+    PID=$(cat $PID_FILE)
+    if ps -p $PID > /dev/null; then
+        kill $PID
+        rm -f $PID_FILE
+        echo "Application stopped."
+    else
+        echo "Process with PID $PID not found. Removing stale PID file."
+        rm -f $PID_FILE
+    fi
 else
-    echo "No pid file found. Is the application running?"
+    echo "No PID file found. Attempting to find the process manually."
+    PID=$(ps -ef | grep "[p]ython $APP_DIR/app.py" | awk '{print $2}')
+    if [ -n "$PID" ]; then
+        kill $PID
+        echo "Application stopped. PID was $PID."
+    else
+        echo "Application not running."
+    fi
 fi
-EOF
